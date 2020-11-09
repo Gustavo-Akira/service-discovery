94e45551a4175efebe6e8490ef1219c22af9a8f9
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2020-11-08 23:10:42 -0300

    Discovering services

---
+++ src/test/java/br/com/gustavoakira/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavoakira.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
